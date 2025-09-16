c5c2c34e8f81970ecd578bb07823195a4a50e39d
minhazulabidin <minhajulabedin648@gmail.com>
2025-09-16 13:18:13 +0600

    nav after done

--- src/Components/Utilities/Header.jsx
+++ src/Components/Utilities/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Container from './Container'
-import logo from '../../../public/Logo.png'
+import logo from '/public/Logo.png'
 import { CiSearch } from 'react-icons/ci'
 import { RiDeleteBin6Line } from 'react-icons/ri'
 import { AiOutlineUser } from 'react-icons/ai'
+import Flex from './Flex'
 
 const Header = () => {
     const navItems = [
@@ -36,19 +37,26 @@ const Header = () => {
 
 
     return (
-        <header>
+        <header className='mt-5'>
             <nav>
                 <Container>
-                    <div>
+                    <div className='flex justify-between items-center'>
                         <img src={logo} alt="" />
-                        <ul>
+                        <ul className='flex gap-10'>
                             {
-                                navItems.map((item) => <li key={item?.id}><a href={item?.href}>{item?.name}</a></li>)
+                                navItems.map((item) => <li
+                                    className='relative after:absolute after:h-[2px] after:w-0 after:bg-black after:bottom-0 after:left-0 hover:after:w-full after:transition-all hover:font-semibold'
+                                    key={item?.id}
+                                >
+                                    <a href={item?.href}>{item?.name}</a>
+                                </li>)
                             }
                         </ul>
-                        <div>
+                        <div className='flex gap-5 text-2xl'>
                             <CiSearch />
-                            <RiDeleteBin6Line />
+                            <div className='after:content-[""] after:w-[10px] after:h-[10px] after:bg-red-500 relative after:absolute after:top-[-8px] after:right-[-5px] after:rounded-full'>
+                                <RiDeleteBin6Line />
+                            </div>
                             <AiOutlineUser />
                         </div>
                     </div>
