cc8869fbd72049a7a8b6ff911668bc79ea345601
introtoxicXXl <minhajulabedin648@gmail.com>
2025-09-20 22:05:17 +0600

    collection responsive done

--- src/Components/Pages/Home/Collection.jsx
+++ src/Components/Pages/Home/Collection.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Flex from '../../Utilities/Flex'
 import Container from '../../Utilities/Container'
 import sofa from '../../../assets/sofa.png'
@@ -7,23 +6,36 @@ import chair from '../../../assets/chair.png'
 const Collection = () => {
     return (
         <section className="relative mt-[66px]">
-            <div className="absolute top-0 right-0 w-1/2 h-full bg-first -z-10"></div>
+            {/* Background side */}
+            <div className="absolute lg:top-0 lg:right-0 lg:w-1/2 w-full lg:h-full h-1/2 bg-first -z-10"></div>
+            
             <Container>
-                <Flex className="justify-between">
-                    <Flex className='w-2/4 justify-between items-center py-[74px] px-[50px]'>
-                        <div className='space-y-[41px]'>
-                            <h2 className='text-typography text-3xl'>House Product</h2>
-                            <p className='text-first text-typography text-[25px]'>Browse More Collection</p>
+                <Flex className="flex-col-reverse lg:flex-row lg:justify-between">
+                    
+                    {/* Left box */}
+                    <Flex className="lg:w-2/4 w-full flex-col-reverse sm:flex-row justify-between items-center lg:py-[74px] py-8 lg:px-[50px] px-4 gap-6">
+                        <div className="space-y-5 lg:space-y-[41px] text-center sm:text-left">
+                            <h2 className="text-typography lg:text-3xl text-xl">House Product</h2>
+                            <p className="text-first text-typography lg:text-[25px] text-base">
+                                Browse More Collection
+                            </p>
                         </div>
-                        <img src={chair} alt="Chair" />
+                        <img src={chair} alt="Chair" className="max-w-[150px] sm:max-w-[200px] lg:max-w-[250px]" />
                     </Flex>
-                    <Flex className='w-2/4 justify-between items-center py-[74px] px-[50px]'>
-                        <div className='space-y-[41px]'>
-                            <h2 className='text-3xl text-black text-typography'>Office Furnicer</h2>
-                            <p className='text-[25px] text-second'>Browse More Collection</p>
+
+                    {/* Right box */}
+                    <Flex className="lg:w-2/4 w-full flex-col-reverse sm:flex-row justify-between items-center lg:py-[74px] py-8 lg:px-[50px] px-4 gap-6">
+                        <div className="space-y-5 lg:space-y-[41px] text-center sm:text-left">
+                            <h2 className="lg:text-3xl text-xl text-black text-typography">
+                                Office Furnicer
+                            </h2>
+                            <p className="lg:text-[25px] text-base text-second">
+                                Browse More Collection
+                            </p>
                         </div>
-                        <img src={sofa} alt="Sofa"/>
+                        <img src={sofa} alt="Sofa" className="max-w-[150px] sm:max-w-[200px] lg:max-w-[250px]" />
                     </Flex>
+
                 </Flex>
             </Container>
         </section>
